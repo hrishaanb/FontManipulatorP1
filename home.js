3ae264dead34554c97253c5af40afd8083045d36
hrishaanb <105030234+hrishaanb@users.noreply.github.com>
2023-07-29 16:43:02 -0700

    Delete home.js

--- home.js
+++
@@ -1,9 +0,0 @@
-var input = "";
-function nextPage () {
-    input = document.getElementById("input").value;
-    if (input == "") {
-        input = "Nothing";
-    }
-    console.log(input);
-    window.location = "index.html";
-}
