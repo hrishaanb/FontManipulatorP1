e0e7dc1d79e2893d424d2176e9390d99fcdfd9c2
hrishaanb <105030234+hrishaanb@users.noreply.github.com>
2023-07-30 12:18:19 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 function setup () {
     canvas = createCanvas(400, 400);
-    canvas.center();
+    canvas.position(575, 225);
+    //video = createCapture(VIDEO);
+    //video.size(500, 500);
+    //video.position(30, 200);
+}
+var input = "";
+function nextPage () {
+    input = document.getElementById("input").value;
+    if (input == "") {
+        input = "Nothing";
+    }
+    console.log(input);
 }
